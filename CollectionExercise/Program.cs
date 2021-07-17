28f29dbca292cbc8edd44fc8770ad9bbb3aa66bf
Rishav Kedia <rishav267@gmail.com>
2021-07-17 10:44:57 +0530

    Add project files.

---
+++ CollectionExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,int> dict = new Dictionary<string,int>();
+            for(int i=0;i<args.Length;i++)
+            {
+                if(dict.ContainsKey(args[i]))
+                {
+                    dict[args[i]]++;
+                }
+                else
+                {
+                    dict[args[i]] = 1;
+                }
+            }
+            //dict = dict.OrderBy(Key => Key.Key);
+            foreach(KeyValuePair<string,int> k in dict.OrderBy(Key => Key.Key))
+            {
+                Console.WriteLine("key = {0}, Value = {1}", k.Key, k.Value);
+            }
+        }
+    }
+}
